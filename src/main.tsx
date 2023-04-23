aca082592b64376a0cf644ac47b12af024969874
Bereke Kabdi <kabdibereke@gmail.com>
2023-04-23 13:55:49 +0600

    fix bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './store/store.ts'
+import { store } from './store/store'
+import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
